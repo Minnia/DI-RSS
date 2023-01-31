d64f65a8e82aea907340d5418759e4c14f277501
Minnia <jasminestockholm@gmail.com>
2023-02-01 00:10:28 +0100

    init styling for article

---
+++ di-rss/src/screens/News/components/Article/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ArticleContainer = styled.div`
+  height: 200px;
+  width: 250px;
+  border: 1px solid black;
+  padding: 8px;
+  margin: 8px;
+`;
+
+export const ArticleTitle = styled.span`
+  font-weight: bold;
+  color: black;
+`;
+
+export const ArticleAuthor = styled.h4`
+  color: black;
+`;
+
+export const ArticlePublishingDate = styled(ArticleAuthor)``;
