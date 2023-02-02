62179af074e4f7f19705e754eb759ec6544b7148
Minnia <jasminestockholm@gmail.com>
2023-02-03 00:10:40 +0100

    fix styling

--- di-rss/src/screens/News/components/Article/styled.js
+++ di-rss/src/screens/News/components/Article/styled.js
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 import { colors, variables } from "../../../../theme";
 
-const { baseline, fontWeightBold, fontWeightRegular, newsSnippetHeight } =
-  variables;
+const { baseline, fontWeightBold, fontWeightRegular } = variables;
 
 export const ArticleContainer = styled.div`
-  height: ${newsSnippetHeight}px;
-  width: 70%;
   // chose to not make box-shadow a variable, as it's not commonly used everywhere
   box-shadow: 0 1px 2px 0 rgb(26 26 26 / 15%);
   border-top: ${variables.baseline * 0.5}px solid ${colors.diRed};
   box-sizing: border-box;
   padding: ${baseline}px;
   margin: ${baseline}px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 export const ImageContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+  display: block;
+  position: relative;
+  padding-top: 56.25%;
+  width: 100%;
 `;
 
 export const ArticleText = styled.span`
@@ -30,10 +31,15 @@ export const ArticleText = styled.span`
   margin-bottom: ${baseline * 0.5}px;
   padding-right: ${baseline * 0.5}px;
 `;
+
+export const ArticleTitle = styled(ArticleText)`
+  min-height: 40px;
+`;
+
 export const ArticleTextOuterContainer = styled.div`
-  position: relative;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
+  align-items: flex-end;
 `;
 export const ArticleTextInnerContainer = styled(ArticleTextOuterContainer)`
   flex: 1;
@@ -43,6 +49,7 @@ export const ArticleTextInnerContainer = styled(ArticleTextOuterContainer)`
 export const ArticleImage = styled.img`
   height: 100%;
   width: 100%;
-  position: relative;
-  padding-bottom: ${variables.baseline}px;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
