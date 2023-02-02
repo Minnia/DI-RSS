65eab46396787671eae0b1cd4216bead9c69f33f
Minnia <jasminestockholm@gmail.com>
2023-02-02 16:47:29 +0100

    add styling for news container and fn getGridSize to determine size of grid when dimensions change

--- di-rss/src/screens/News/styled.js
+++ di-rss/src/screens/News/styled.js
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
+const getGridSize = (size) => {
+  // console.log("size", size.dynamicWidth);
+  // console.log(size.dynamicWidth <= 320);
+  // console.log(size.dynamicWidth >= 320 && size.dynamicWidth <= 425);
+  // console.log(size.dynamicWidth >= 768);
+
+  switch (true) {
+    case size.dynamicWidth <= 320:
+      return "auto";
+    case size.dynamicWidth > 320 && size.dynamicWidth < 768:
+      return "auto auto";
+    case size.dynamicWidth >= 768:
+      return "auto auto auto";
+    default:
+      return "1fr 1fr 1fr";
+  }
+};
+
 export const NewsContainer = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-gap: 8px;
+  grid-template-columns: ${({ size }) => getGridSize(size)};
   height: 100%;
+  padding: 16px;
 `;
