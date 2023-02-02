1357e4d72990e201e37d69e6bee225ae50cea003
Minnia <jasminestockholm@gmail.com>
2023-02-02 20:20:03 +0100

    init Header and styling

---
+++ di-rss/src/components/common/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styled";
+
+const Header = () => {
+  return <S.StyledHeader>Välkommen till DI</S.StyledHeader>;
+};
+
+export default Header;
---
+++ di-rss/src/components/common/Header/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "../../../theme";
+
+export const StyledHeader = styled.h1`
+  color: ${colors.diRed};
+  margin-left: 24px;
+  border-bottom: 4px solid ${colors.diRed};
+`;
