2365f73210103474161b1ad1effb64ef8c5fc84f
Minnia <jasminestockholm@gmail.com>
2023-02-03 00:33:41 +0100

    add comments

--- di-rss/src/screens/News/hooks/useNews.js
+++ di-rss/src/screens/News/hooks/useNews.js
@@ -10,6 +10,12 @@ export const useNews = () => {
     getNews();
   }, []);
 
+  /** The getNews() function fetches the data and converts the XML we get.
+   * It then maps over all the converted data and returns child.children.
+   * Then, we once again iterate and flatten the array, to then filter through the children
+   * and return filtered results with data.
+   * We map through and flatten the array, and then sort our flattened results in an ascending order.
+   */
   const getNews = async () => {
     setLoading(true);
 
@@ -18,12 +24,12 @@ export const useNews = () => {
     const convertXml = new XMLParser().parseFromString(data);
     const getAll = convertXml.children.map((child) => child.children);
 
-    const getAllNews = getAll.map((child, i) =>
+    const getAllNews = getAll.map((child) =>
       child.flat().filter((c) => c.children.length > 0)
     );
 
-    const flattenNewsArr = getAllNews.map((t) =>
-      t.flat().map((tes) => tes.children)
+    const flattenNewsArr = getAllNews.map((articles) =>
+      articles.flat().map((article) => article.children)
     );
     const [flattened] = flattenNewsArr.map((t) => t);
     const sortNewsAsc = flattened.sort((a, b) => {
@@ -35,6 +41,14 @@ export const useNews = () => {
       return new Date(bPubDate).getTime() - new Date(aPubDate).getTime();
     });
 
+    /** formattedDate maps through the array of news twice. Once, to map through
+     * each item and once to map through the element of the item. After, we're checking
+     * if there are children within an element. If so, we map through these and check if there
+     * is a thumbnail, and if there is we return an array with the key and value (which here is the url).
+     * Otherwise, we'll return the key and value of the child.
+     * In the end, we return the article keys and values as an object,
+     * We set the data to news and set loading to false
+     */
     const formattedData = sortNewsAsc.map((item) => {
       const articleKeyValues = item.map((element) => {
         if (element.children && element.children.length > 0) {
