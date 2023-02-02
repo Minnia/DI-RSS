7a0b4dd1d2234298329548e5a5bdab86935185fe
Minnia <jasminestockholm@gmail.com>
2023-02-02 16:47:49 +0100

    redo some styled components to reusable ones, add props

--- di-rss/src/screens/News/components/Article/Article.jsx
+++ di-rss/src/screens/News/components/Article/Article.jsx
@@ -2,21 +2,30 @@ import React from "react";
 import * as S from "./styled";
 
 const Article = ({ article }) => {
-  console.log(article);
+  const thumbNail =
+    article?.content && article?.content["media:thumbnail"].split("?")[0];
+
+  console.log(new Date(article.pubDate).toLocaleString());
+
+  const publishingDate = new Date(article.pubDate).toLocaleString();
+
   return (
     <S.ArticleContainer>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
+      <S.ArticleText
+        fontWeight="bold"
+        style={{ display: "flex", justifyContent: "center" }}
       >
-        <S.ArticleTitle>{article.title}</S.ArticleTitle>
-      </div>
-      <div>
-        <S.ArticleTitle>{article.pubDate}</S.ArticleTitle>
-      </div>
+        {article.title}
+      </S.ArticleText>
+
+      <S.ImageContainer>
+        {article?.content && <S.ArticleImage alt="" src={thumbNail} />}
+      </S.ImageContainer>
+
+      <S.ArticleText fontWeight="bold">{publishingDate}</S.ArticleText>
+      <S.ArticleText fontWeight="regular">
+        {article["dc:creator"]}
+      </S.ArticleText>
     </S.ArticleContainer>
   );
 };
--- di-rss/src/screens/News/components/Article/styled.js
+++ di-rss/src/screens/News/components/Article/styled.js
@@ -1,21 +1,30 @@
 import styled from "styled-components";
 
 export const ArticleContainer = styled.div`
-  height: 200px;
-  width: 250px;
-  border: 1px solid black;
+  height: 250px;
+  width: 80%;
+  box-shadow: 2px 5px #ffffff;
   border-radius: 5%;
+  box-sizing: border-box;
   padding: 8px;
   margin: 8px;
+  background-color: #ffffff;
+`;
+export const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
 
-export const ArticleTitle = styled.span`
-  font-weight: bold;
+export const ArticleText = styled.span`
+  font-weight: ${({ fontWeight }) =>
+    fontWeight === "bold" ? "bold" : "regular"};
   color: black;
 `;
 
-export const ArticleAuthor = styled.h4`
-  color: black;
-`;
+export const ArticleImage = styled.img`
+  height: 200px;
+  width: 500;
 
-export const ArticlePublishingDate = styled(ArticleAuthor)``;
+  padding-bottom: 8px;
+`;
