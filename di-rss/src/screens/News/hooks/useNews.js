f41191789b902f52cbda3eda93383f75156bc95c
Minnia <jasminestockholm@gmail.com>
2023-02-03 14:24:41 +0100

    extract logic to fn

--- di-rss/src/screens/News/hooks/useNews.js
+++ di-rss/src/screens/News/hooks/useNews.js
@@ -2,6 +2,49 @@ import { useEffect, useState } from "react";
 import XMLParser from "react-xml-parser";
 import { getNewsFromRSS } from "../../../util/api/di.routes";
 
+const parseXMLToJSON = (data) => {
+  const convertXml = new XMLParser().parseFromString(data);
+  const getAll = convertXml.children.map((child) => child.children);
+
+  const getAllNews = getAll.map((child) =>
+    child.flat().filter((c) => c.children.length > 0)
+  );
+
+  const [flattenNewsArr] = getAllNews.map((articles) =>
+    articles.flat().map((article) => article.children)
+  );
+  return flattenNewsArr;
+};
+
+const formatJSONToNews = (item) => {
+  const articleKeyValues = item.map((element) => {
+    if (element.children && element.children.length > 0) {
+      const childrenKeyValues = element.children.map((child) => {
+        if (child.name.includes("thumbnail")) {
+          return [child.name, child.attributes.url];
+        }
+        return [child.name, child.value];
+      });
+      const keyValues = ["content", Object.fromEntries(childrenKeyValues)];
+
+      return keyValues;
+    }
+    return [element.name, element.value];
+  });
+
+  return Object.fromEntries(articleKeyValues);
+};
+
+const sortNews = (arr) =>
+  arr.sort((a, b) => {
+    const aIndex = a.findIndex((item) => item.name === "pubDate");
+    const bIndex = b.findIndex((item) => item.name === "pubDate");
+    const aPubDate = a[aIndex].value;
+    const bPubDate = b[bIndex].value;
+
+    return new Date(bPubDate).getTime() - new Date(aPubDate).getTime();
+  });
+
 export const useNews = () => {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -10,39 +53,6 @@ export const useNews = () => {
     getNews();
   }, []);
 
-  const parseXMLToJSON = (data) => {
-    const convertXml = new XMLParser().parseFromString(data);
-    const getAll = convertXml.children.map((child) => child.children);
-
-    const getAllNews = getAll.map((child) =>
-      child.flat().filter((c) => c.children.length > 0)
-    );
-
-    const [flattenNewsArr] = getAllNews.map((articles) =>
-      articles.flat().map((article) => article.children)
-    );
-    return flattenNewsArr;
-  };
-
-  const formatJSONToNews = (item) => {
-    const articleKeyValues = item.map((element) => {
-      if (element.children && element.children.length > 0) {
-        const childrenKeyValues = element.children.map((child) => {
-          if (child.name.includes("thumbnail")) {
-            return [child.name, child.attributes.url];
-          }
-          return [child.name, child.value];
-        });
-        const keyValues = ["content", Object.fromEntries(childrenKeyValues)];
-
-        return keyValues;
-      }
-      return [element.name, element.value];
-    });
-
-    return Object.fromEntries(articleKeyValues);
-  };
-
   const getNews = async () => {
     setLoading(true);
 
@@ -50,16 +60,9 @@ export const useNews = () => {
 
     const parseData = parseXMLToJSON(data);
 
-    const sortNewsAsc = parseData.sort((a, b) => {
-      const aIndex = a.findIndex((item) => item.name === "pubDate");
-      const bIndex = b.findIndex((item) => item.name === "pubDate");
-      const aPubDate = a[aIndex].value;
-      const bPubDate = b[bIndex].value;
+    const sortedNews = sortNews(parseData);
 
-      return new Date(bPubDate).getTime() - new Date(aPubDate).getTime();
-    });
-
-    const formattedData = sortNewsAsc.map((item) => formatJSONToNews(item));
+    const formattedData = sortedNews.map((item) => formatJSONToNews(item));
 
     setNews(formattedData);
     return setLoading(false);
