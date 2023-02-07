1f048e08afcd4e737b28483ad0c330bce9d2aa40
Minnia <jasminestockholm@gmail.com>
2023-02-07 18:44:54 +0100

    add routes

--- di-rss/src/util/api/di.routes.js
+++ di-rss/src/util/api/di.routes.js
@@ -4,3 +4,11 @@ export const getNewsFromRSS = async () => {
   const { data } = await axios.get("https://www.di.se/rss");
   return data;
 };
+export const getExpressenNews = async () => {
+  const { data } = await axios.get("https://feeds.expressen.se/nyheter/");
+  return data;
+};
+export const getGTNews = async () => {
+  const { data } = await axios.get("https://feeds.expressen.se/gt");
+  return data;
+};
